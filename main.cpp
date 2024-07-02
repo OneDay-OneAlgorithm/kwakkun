db25916fcb9ef712c8aaeccbc5f75c1f5e35ff0e
kwak kun <gael.urssu@gmail.com>
2024-07-02 12:54:42 +0900

    2024/07/02 :: 이항계수 1

--- main.cpp
+++ main.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
+int factorial(int k) {
+    int sum = 1;
+    while (k != 0) {
+        sum *= k;
+        k--;
+    }
+    return sum;
+}
+
+int choice(int n, int k) {
+    return factorial(n) / factorial(n - k) / factorial(k);
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n, k;
-    vector<int> arr;
-    int counter = 0;
-
     cin >> n >> k;
-    for (int i = 0; i < n; ++i) {
-        int a;
-        cin >> a;
-        arr.push_back(a);
-    }
-
-    int curr = arr.size() - 1;
-    while (k != 0) {
-        if (arr[curr] <= k) {
-            k -= arr[curr];
-            counter++;
-        } else {
-            curr--;
-        }
-    }
-    cout << counter;
+    cout << choice(n, k);
 }
