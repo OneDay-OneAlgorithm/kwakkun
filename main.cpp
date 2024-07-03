e89f8347b2445315b34fce360108325a343b666f
kwak kun <gael.urssu@gmail.com>
2024-07-03 16:54:07 +0900

    2024/07/03 :: 수식 고치기

--- main.cpp
+++ main.cpp
@@ -1,25 +1,28 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+int N;
+char target, A[101010];
 
-int factorial(int k) {
-    int sum = 1;
-    while (k != 0) {
-        sum *= k;
-        k--;
+int solve(int n, char t) {
+    if (n == 1) return A[1] == t ? 0 : 1;
+    if (t == 'T') {
+        if (A[n] == 'T' && A[n - 1] == '&') return min(1, solve(n - 2, 'T'));
+        if (A[n] == 'T' && A[n - 1] == '|') return 0;
+        if (A[n] == 'F' && A[n - 1] == '&') return min(2, 1 + solve(n - 2, 'T'));
+        if (A[n] == 'F' && A[n - 1] == '|') return min(1, solve(n - 2, 'T'));
+    } else if (t == 'F') {
+        if (A[n] == 'T' && A[n - 1] == '&') return min(1, solve(n - 2, 'F'));
+        if (A[n] == 'T' && A[n - 1] == '|') return min(2, 1 + solve(n - 2, 'F'));
+        if (A[n] == 'F' && A[n - 1] == '&') return 0;
+        if (A[n] == 'F' && A[n - 1] == '|') return min(1, solve(n - 2, 'F'));
     }
-    return sum;
-}
-
-int choice(int n, int k) {
-    return factorial(n) / factorial(n - k) / factorial(k);
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n, k;
-    cin >> n >> k;
-    cout << choice(n, k);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> A[i];
+    cin >> target;
+    cout << solve(N, target);
 }
