deda4f572b36b97b3209e874d7b888c65b5e3782
kwak kun <gael.urssu@gmail.com>
2024-07-07 21:28:23 +0900

    2024/07/07 :: 이항계수2

--- main.cpp
+++ main.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-#define ll long long
-using lll = __int128;
 using namespace std;
-
-ll fpow(lll a, lll b, lll c) {
-    ll res = 1;
-    while (b > 0) {
-        if (b % 2 == 1) {
-            res = (res * a) % c;
-        }
-        a = (a * a) % c;
-        b /= 2;
-    }
-    return res;
-}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    ll a, b, c;
-    cin >> a >> b >> c;
-    cout << fpow(a, b, c) << '\n';
+    int n, k;
+    cin >> n >> k;
+
+    vector<vector<int>> v;
+
+    for (int i = 0; i < n + 1; i++) {
+        vector<int> temp;
+        temp.push_back(1);
+        for (int j = 1; j <= i; j++) {
+            temp.push_back((v[i - 1][j - 1] + v[i - 1][j]) % 10007);
+        }
+        temp.push_back(0);
+        v.push_back(temp);
+    }
+
+//    for (auto i: v) {
+//        for (auto j: i) {
+//            cout << j << " ";
+//        }
+//        cout << endl;
+//    }
+
+    cout << v[n][k] % 10007 << endl;
+
+    return 0;
 }
