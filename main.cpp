ec95f21dc37332036985810b2a6d2e0b0d9b57e9
kwak kun <gael.urssu@gmail.com>
2024-07-21 11:58:39 +0900

    2024/07/21 :: 최솟값 찾기

--- main.cpp
+++ main.cpp
@@ -1,77 +1,34 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-#define ll long long
 
-int n;
-vector<ll> v;
-vector<ll> input;
-vector<ll> tree;
-
-void init(int node = 1, int start = 0, int end = n - 1) {
-    if (start == end) {
-        tree[node] = 0;
-    } else {
-        int mid = (start + end) / 2;
-        init(node * 2, start, mid);
-        init(node * 2 + 1, mid + 1, end);
-        tree[node] = tree[node * 2] + tree[node * 2 + 1];
+struct Compare {
+    bool operator()(pair<int, int> const &p1, pair<int, int> const &p2) {
+        return p1.second > p2.second;
     }
-}
+};
 
-int query(int left, int right, int node = 1, int start = 0, int end = n - 1) {
-    if (left > end || right < start) {
-        return 0;
-    }
+int n, l;
+// fisrt: index, second: value
+priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
 
-    if (left <= start && end <= right) {
-        return tree[node];
-    }
-
-    int mid = (start + end) / 2;
-    return query(left, right, node * 2, start, mid) + query(left, right, node * 2 + 1, mid + 1, end);
-}
-
-void update(int idx, int value, int node = 1, int start = 0, int end = n - 1) {
-    if (idx < start || idx > end) {
-        return;
-    }
-
-    if (start == end) {
-        tree[node] += value;
-        return;
-    }
-
-    int mid = (start + end) / 2;
-    update(idx, value, node * 2, start, mid);
-    update(idx, value, node * 2 + 1, mid + 1, end);
-    tree[node] = tree[node * 2] + tree[node * 2 + 1];
-}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> n;
-    v.resize(n);
-    input.resize(n);
-
+    cin >> n >> l;
     for (int i = 0; i < n; i++) {
-        cin >> input[i];
+        int x;
+        cin >> x;
+
+        pq.emplace(i, x);
+        while (pq.top().first <= i - l) {
+            pq.pop();
+        }
+        cout << pq.top().second << " ";
+
     }
 
-    int h = (int) ceil(log2(n));
-    int tree_size = (1 << (h + 1));
-    tree.resize(tree_size);
-
-    init();
-
-    ll result = 0;
-    for (int i = 0; i < n; i++) {
-        update(input[i] - 1, 1);
-        result += query(input[i], n - 1);
-    }
-
-    cout << result << endl;
     return 0;
 }
