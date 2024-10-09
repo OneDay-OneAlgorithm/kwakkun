e98689f2510efb7491163faa3f7633f9b22bd1a8
kwak kun <gael.urssu@gmail.com>
2024-10-09 10:57:45 +0900

    2024/10/08 :: Animal Farm

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <bits/stdc++.h>
 
+#define ll long long
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
-    int K;
-    cin >> K;
+    int n;
+    cin >> n;
+    vector<pair<string, int>> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].first >> a[i].second;
+    }
 
-    cout << (K - (N - 1)) / N << endl;
+    int max_pig_v = 0;
+    for (auto x: a) {
+        if (x.first == "pig") max_pig_v = max(max_pig_v, x.second);
+    }
+
+    ll result = 0;
+    for(auto x : a) {
+        if (x.first != "pig" && x.second < max_pig_v) {
+            result += x.second;
+        }
+    }
+
+    cout << result + max_pig_v << endl;
 }
