5a311481fa081eb991e5bf18f22628d0b60a01a4
kwak kun <gael.urssu@gmail.com>
2025-01-08 13:35:41 +0900

    2025/01/08 :: 최소 스패닝 트리 복습

--- main.cpp
+++ main.cpp
@@ -1,33 +1,28 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef pair<int, int> WeightEdge; // first: weight, second: vertex
-typedef vector<vector<WeightEdge>> Graph;
+typedef pair<int, int> WeightedEdge;
+typedef vector<vector<WeightedEdge >> Graph;
 
 Graph graph;
 
 int prim() {
     int startVertex = 1;
-    priority_queue<WeightEdge, vector<WeightEdge>, greater<>> pq;
+    priority_queue<WeightedEdge, vector<WeightedEdge>, greater<>> pq;
     vector<bool> visited(graph.size(), false);
 
     pq.emplace(0, startVertex);
 
     int totalWeight = 0;
     while (!pq.empty()) {
-        int w = pq.top().first;
-        int u = pq.top().second;
+        auto [weight, vertex] = pq.top();
         pq.pop();
-
-        if (visited[u]) continue;
-        visited[u] = true;
-        totalWeight += w;
-
-        for (auto &edge: graph[u]) {
-            int weight = edge.first; // weight
-            int v = edge.second; // vertex
+        if (visited[vertex]) continue;
+        visited[vertex] = true;
+        totalWeight += weight;
+        for (auto [w, v]: graph[vertex]) {
             if (!visited[v]) {
-                pq.emplace(weight, v);
+                pq.emplace(w, v);
             }
         }
     }
@@ -39,12 +34,12 @@ int main() {
     int V, E;
     cin >> V >> E;
     graph.resize(V + 1);
-    for (int i = 0; i < E; i++) {
+    for (int i = 0; i < E; ++i) {
         int u, v, w;
         cin >> u >> v >> w;
         graph[u].emplace_back(w, v);
         graph[v].emplace_back(w, u);
     }
 
-    cout << prim();
+    cout << prim() << endl;
 }
