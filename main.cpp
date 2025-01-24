9b7069d9e8af58250d4abc7f72cc553bf11e92c1
kwak kun <gael.urssu@gmail.com>
2025-01-24 13:37:27 +0900

    2025/01/24 :: 중앙값 구하기

--- main.cpp
+++ main.cpp
@@ -2,37 +2,49 @@
 
 using namespace std;
 
-int gcd(int a, int b) {
-    if (b == 0) {
-        return a;
+class Median {
+private:
+    priority_queue<int> maxHeap; // 작은 값들
+    priority_queue<int, vector<int>, greater<>> minHeap; // 큰 값들
+public:
+    void insert(int x) {
+        if (maxHeap.empty() || x <= maxHeap.top()) {
+            maxHeap.push(x);
+        } else {
+            minHeap.push(x);
+        }
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
     }
-    return gcd(b, a % b);
-}
 
-int sectionGcd(vector<int> A) {
-    int res = A[0];
-    for (int i = 1; i < A.size(); i++) {
-        res = gcd(res, A[i]);
+    int query() {
+        return maxHeap.top();
     }
-    return res;
-}
-
-int divide(vector<int> A) {
-    if (A.size() == 1) {
-        return A[0];
-    }
-    int m = A.size() / 2;
-    int left = sectionGcd(vector<int>(A.begin(), A.begin() + m)) + divide(vector<int>(A.begin() + m, A.end()));
-    int right = sectionGcd(vector<int>(A.begin() + m, A.end())) + divide(vector<int>(A.begin(), A.begin() + m));
-    return max(left, right);
-}
+};
 
 int main() {
-    int N;
-    cin >> N;
-    vector<int> A(N);
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
+    int T;
+    cin >> T;
+    while (T--) {
+        int M;
+        cin >> M;
+        Median central = Median();
+        vector<int> arr;
+        for (int i = 1; i <= M; i++) {
+            int x;
+            cin >> x;
+            central.insert(x);
+            if (i % 2 == 1) { arr.push_back(central.query()); }
+        }
+        cout << arr.size() << endl;
+        for (int i: arr) {
+            cout << i << " ";
+        }
+        cout << endl;
     }
-    cout << divide(A);
 }
